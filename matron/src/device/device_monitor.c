80010e17dc087e67e105c86133eea65e4b65ca92
ezra buchla <emb@catfact.net>
2021-05-01 17:44:28 -0700

    refactor device_monitor, support grid-st

--- matron/src/device/device_monitor.c
+++ matron/src/device/device_monitor.c
@@ -17,53 +17,72 @@
 #include <unistd.h>
 
 #include "device.h"
-#include "device_crow.h"
 #include "device_hid.h"
 #include "device_list.h"
 #include "device_monome.h"
+//#include "device_tty.h"
+
 #include "events.h"
 
 #define SUB_NAME_SIZE 32
 #define NODE_NAME_SIZE 128
 #define WATCH_TIMEOUT_MS 100
 
-static enum {
-    DEV_CLASS_TTY,
-    DEV_CLASS_ACM,
-    DEV_CLASS_HID,
-    DEV_CLASS_MIDI
+// enumerate unix files to watch
+enum {
+    DEV_FILE_TTY   = 0,
+    DEV_FILE_INPUT = 1,
+    DEV_FILE_SOUND = 2,
+    DEV_FILE_NONE  = 3,
+};
+#define DEV_FILE_COUNT 3
+
+struct udev_monitor *mon[DEV_FILE_COUNT];
+
+static const char *dev_file_name[] = {
+    "tty",
+    "input",
+    "sound",
+    "none"
 };
 
-struct watch {
-    // subsystem name to use as a filter on udev_monitor
-    const char sub_name[NODE_NAME_SIZE];
-    // glob pattern for checking the device node
-    const char node_pattern[NODE_NAME_SIZE];
-    // udev monitor
-    struct udev_monitor *mon;
-};
+
+/*
+-static struct watch watches[DEV_TYPE_COUNT_PHYSICAL] = {
+-    {.sub_name = "tty", .node_pattern = "/dev/ttyUSB*"},
+-    {.sub_name = "input", .node_pattern = "/dev/input/event*"},
+-    {.sub_name = "sound", .node_pattern = "/dev/snd/midiC*D*"},
+-    {.sub_name = "crow", .node_pattern = "/dev/ttyACM*"}};
+-
+ // file descriptors to watch/poll
+*/
+
 
 //-------------------------
 //----- static variables
 
-// watchers
-// FIXME: these names / paths are really arbitrary.
-static struct watch watches[DEV_TYPE_COUNT_PHYSICAL] = {
-    {.sub_name = "tty", .node_pattern = "/dev/ttyUSB*"},
-    {.sub_name = "input", .node_pattern = "/dev/input/event*"},
-    {.sub_name = "sound", .node_pattern = "/dev/snd/midiC*D*"},
-    {.sub_name = "crow", .node_pattern = "/dev/ttyACM*"}};
 
 // file descriptors to watch/poll
-struct pollfd pfds[DEV_TYPE_COUNT_PHYSICAL];
+struct pollfd pfds[DEV_FILE_COUNT];
 // thread for polling all the watched file descriptors
 pthread_t watch_tid;
 
 //--------------------------------
 //--- static function declarations
+static int rm_dev(struct udev_device *dev, int dev_file);
+static int rm_dev_tty(struct udev_device *dev, const char *node);
+
+static void add_dev(struct udev_device *dev, int dev_file);
+static void add_dev_tty(struct udev_device *dev);
+static void add_dev_input(struct udev_device *dev);
+static void add_dev_sound(struct udev_device *dev);
+
+static int is_dev_monome_grid(struct udev_device *dev);
+
 static void *watch_loop(void *data);
-static void handle_device(struct udev_device *dev);
-static device_t check_dev_type(struct udev_device *dev);
+
+//static device_t check_dev_type(struct udev_device *dev);
+
 // try to get MIDI device name from ALSA
 // returns a newly-allocated string (may be NULL)
 static const char *get_alsa_midi_node(struct udev_device *dev);
@@ -71,9 +90,12 @@ static const char *get_alsa_midi_node(struct udev_device *dev);
 // returns a newly-allocated string (may be NULL)
 static const char *get_device_name(struct udev_device *dev);
 
+static inline void print_watch_error(const char *msg, int file_idx) {
+    fprintf(stderr, "error: %s on subsystem %s", msg, dev_file_name[file_idx]);
+}
+
 //--------------------------------
 //---- extern function definitions
-
 void dev_monitor_init(void) {
     struct udev *udev = NULL;
     pthread_attr_t attr;
@@ -82,27 +104,25 @@ void dev_monitor_init(void) {
     udev = udev_new();
     assert(udev);
 
-    for (int i = 0; i < DEV_TYPE_COUNT_PHYSICAL; i++) {
-        watches[i].mon = udev_monitor_new_from_netlink(udev, "udev");
-        if (watches[i].mon == NULL) {
-            fprintf(stderr, "failed to start udev_monitor for subsystem %s, pattern %s\n", watches[i].sub_name,
-                    watches[i].node_pattern);
+    for (int fidx = 0; fidx < DEV_FILE_COUNT; ++fidx) {
+	mon[fidx] = NULL;
+	struct udev_monitor *m = udev_monitor_new_from_netlink(udev, "udev");
+        if (m == NULL) {
+	    print_watch_error("couldn't create udev monitor", fidx);
             continue;
         }
-        if (udev_monitor_filter_add_match_subsystem_devtype(watches[i].mon, watches[i].sub_name, NULL) < 0) {
-            fprintf(stderr, "failed to add udev monitor filter for subsystem %s, pattern %s\n", watches[i].sub_name,
-                    watches[i].node_pattern);
+        if (udev_monitor_filter_add_match_subsystem_devtype(m, dev_file_name[fidx], NULL) < 0) {
+	    print_watch_error("couldn't add subsys filter", fidx);
             continue;
         }
-        if (udev_monitor_enable_receiving(watches[i].mon) < 0) {
-            fprintf(stderr, "failed to enable monitor receiving for for subsystem %s, pattern %s\n", watches[i].sub_name,
-                    watches[i].node_pattern);
+        if (udev_monitor_enable_receiving(m) < 0) {
+            print_watch_error("failed to enable monitor", fidx);
             continue;
         }
-
-        pfds[i].fd = udev_monitor_get_fd(watches[i].mon);
-        pfds[i].events = POLLIN;
-    } // end dev type loop
+        pfds[fidx].fd = udev_monitor_get_fd(m);
+        pfds[fidx].events = POLLIN;
+	mon[fidx] = m;
+    }
 
     s = pthread_attr_init(&attr);
     if (s) {
@@ -117,11 +137,50 @@ void dev_monitor_init(void) {
 
 void dev_monitor_deinit(void) {
     pthread_cancel(watch_tid);
-    for (int i = 0; i < DEV_TYPE_COUNT_PHYSICAL; i++) {
-        free(watches[i].mon);
+    for (int fidx = 0; fidx < DEV_FILE_COUNT; ++fidx) {
+	if (mon[fidx] != NULL) { 
+	    free(mon[fidx]);
+	}
     }
 }
 
+// FIXME
+int dev_monitor_scan(void) {
+    struct udev *udev;
+    struct udev_device *dev;
+
+    udev = udev_new();
+    if (udev == NULL) {
+        fprintf(stderr, "device_monitor_scan(): failed to create udev\n");
+        return 1;
+    }
+
+    for (int fidx=0; fidx < DEV_FILE_COUNT; ++fidx) {
+	struct udev_enumerate *ue;
+        struct udev_list_entry *devices, *dev_list_entry;
+	
+	ue = udev_enumerate_new(udev);		
+	udev_enumerate_add_match_subsystem(ue, dev_file_name[fidx]);
+	udev_enumerate_scan_devices(ue);
+	        devices = udev_enumerate_get_list_entry(ue);
+
+        udev_list_entry_foreach(dev_list_entry, devices) {
+            const char *path;
+
+            path = udev_list_entry_get_name(dev_list_entry);
+            dev = udev_device_new_from_syspath(udev, path);
+
+            if (dev != NULL) {
+		add_dev(dev, fidx);
+                udev_device_unref(dev);
+            }
+        }
+	
+        udev_enumerate_unref(ue);
+    }
+    return 0;
+}
+/*
 int dev_monitor_scan(void) {
     struct udev *udev;
     struct udev_device *dev;
@@ -160,17 +219,104 @@ int dev_monitor_scan(void) {
     }
     return 0;
 }
+*/
 
 //-------------------------------
 //--- static function definitions
+int rm_dev(struct udev_device *dev, int dev_file) {     
+    const char *node = udev_device_get_devnode(dev);
+    if (node == NULL ) {
+	return 1;
+    }
+    switch(dev_file) {
+    case DEV_FILE_TTY:
+	rm_dev_tty(dev, node);
+	break;
+    case DEV_FILE_INPUT:
+	dev_list_remove(DEV_TYPE_HID, node);
+	break;
+    case DEV_FILE_SOUND:
+	dev_list_remove(DEV_TYPE_MIDI, node);
+	break;
+    case DEV_FILE_NONE:
+    default:
+	;;
+    }
+    return 0;
+}
+ 
+ int rm_dev_tty(struct udev_device *dev, const char *node) {
+     // TODO
+     (void)dev;
+     (void)node;
+     return 0;
+ }
 
+
+ void add_dev(struct udev_device *dev, int fidx) {
+#if 1
+     // debug:
+     const char *node = udev_device_get_devnode(dev);
+     fprintf(stderr, "adding device: %s\n", node);
+#endif
+     
+     switch(fidx) {
+     case DEV_FILE_TTY:
+	 add_dev_tty(dev);
+	 break;
+     case DEV_FILE_INPUT:
+	 add_dev_input(dev);
+	 break;
+     case DEV_FILE_SOUND:
+	 add_dev_sound(dev);
+	 break;
+     case DEV_FILE_NONE:
+     default:
+	 break;			
+     }
+
+ }
+
+ void add_dev_tty(struct udev_device *dev) {
+     const char *node = udev_device_get_devnode(dev);
+     if (fnmatch("/dev/ttyUSB*", node, 0)) {
+	 // assume this is an "old" monome device
+	 dev_list_add(DEV_TYPE_MONOME, node, get_device_name(dev));
+	 return;
+     }
+     if (is_dev_monome_grid(dev)) {
+	 // device passes vendor/model check for new grids (and clones?)
+	 dev_list_add(DEV_TYPE_MONOME, node, get_device_name(dev));
+     } else {
+	 // FIXME:
+	 // if it's not a grid, we assume it's a crow
+	 dev_list_add(DEV_TYPE_CROW, node, get_device_name(dev));
+     }
+				
+ }
+ 
+ void add_dev_input(struct udev_device *dev) {
+     const char *node = udev_device_get_devnode(dev);
+     dev_list_add(DEV_TYPE_HID, node, get_device_name(dev));
+ }
+ 
+ void add_dev_sound(struct udev_device *dev) {
+     // try to act according to
+     // https://github.com/systemd/systemd/blob/master/rules/78-sound-card.rules
+     const char *alsa_node = get_alsa_midi_node(dev);	 
+     if (alsa_node != NULL) {
+	 dev_list_add(DEV_TYPE_MIDI, alsa_node, get_device_name(dev));
+     }
+ }
+
+ 
 void *watch_loop(void *p) {
     (void)p;
     struct udev_device *dev;
 
     while (1) {
-        if (poll(pfds, DEV_TYPE_COUNT_PHYSICAL, WATCH_TIMEOUT_MS) < 0) {
-            switch (errno) {
+        if (poll(pfds, DEV_FILE_COUNT, WATCH_TIMEOUT_MS) < 0) {
+	    switch (errno) {
             case EINVAL:
                 perror("error in poll()");
                 exit(1);
@@ -180,21 +326,32 @@ void *watch_loop(void *p) {
             }
         }
 
-        // see which monitor has data
-        for (int i = 0; i < DEV_TYPE_COUNT_PHYSICAL; i++) {
-            if (pfds[i].revents & POLLIN) {
-                dev = udev_monitor_receive_device(watches[i].mon);
+        for (int fidx = 0; fidx < DEV_FILE_COUNT; ++fidx) {
+            if (pfds[fidx].revents & POLLIN) {
+                dev = udev_monitor_receive_device(mon[fidx]);
                 if (dev) {
-                    handle_device(dev);
+		    const char *action = udev_device_get_action(dev);
+		    if (action != NULL) {
+			if (strcmp(action, "remove") == 0) {
+			    rm_dev(dev, fidx);
+			} else {
+			    add_dev(dev, fidx);
+			}			
+		    } else {
+			fprintf(stderr, "dev_monitor error: unknown device action\n");
+		    }
                     udev_device_unref(dev);
                 } else {
-                    fprintf(stderr, "no device data from receive_device(). this is an error!\n");
+                    fprintf(stderr, "dev_monitor error: no device data\n");
                 }
             }
         }
     }
 }
 
+
+
+ /*
 void handle_device(struct udev_device *dev) {
     const char *action = udev_device_get_action(dev);
     const char *node = udev_device_get_devnode(dev);
@@ -229,7 +386,7 @@ void handle_device(struct udev_device *dev) {
             device_t t = check_dev_type(dev);
 
             if (t >= 0 && t < DEV_TYPE_COUNT_PHYSICAL) {
-                if (strcmp(action, "add") == 0) {
+                strcmp (if(action, "add") == 0) {
                     dev_list_add(t, node, get_device_name(dev));
                 } else if (strcmp(action, "remove") == 0) {
                     dev_list_remove(t, node);
@@ -239,6 +396,10 @@ void handle_device(struct udev_device *dev) {
     }
 }
 
+ */
+
+ 
+/*
 device_t check_dev_type(struct udev_device *dev) {
     device_t t = DEV_TYPE_INVALID;
     const char *node = udev_device_get_devnode(dev);
@@ -258,7 +419,8 @@ device_t check_dev_type(struct udev_device *dev) {
     }
     return t;
 }
-
+*/
+ 
 // try to get midi device node from udev_device
 const char *get_alsa_midi_node(struct udev_device *dev) {
     const char *subsys;
@@ -302,3 +464,18 @@ const char *get_device_name(struct udev_device *dev) {
 
     return strdup(current_name);
 }
+
+ int is_dev_monome_grid(struct udev_device *dev) {
+	const char *vendor, *model;
+
+	vendor = udev_device_get_property_value(dev, "ID_VENDOR");
+	model = udev_device_get_property_value(dev, "ID_MODEL");
+
+	printf("vendor: %s; model: %s\n\n", vendor, model);
+	
+	if (vendor != NULL && model != NULL) { 
+	    return (strcmp(vendor,"monome")==0) && (strcmp(model,"grid")==0);
+	} else {
+	    return 0;
+	}
+ }
